b7fb179781bae8df0ccc116e5219aa1e7adbdbe0
Fakebelieve <keygunmitchell@gmail.com>
2021-12-09 03:08:42 -0600

    Added User page & Index/Home page with pagination

--- blogz/routes.py
+++ blogz/routes.py
@@ -1,36 +1,30 @@
 from flask import render_template, url_for, flash, redirect, request
 from blogz import app, db, bcrypt
-from blogz.forms import RegistrationForm, LoginForm
+from blogz.forms import RegistrationForm, LoginForm, PostForm
 from blogz.models import User, Post
 from flask_login import login_user, logout_user, current_user, login_required
 
 
-posts = [
-    {
-        'author': 'Key Mitchell',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2016'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2016'
-    },
-]
-
-
 @app.route("/")
-@app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    users = User.query.order_by(User.id.desc())
+    return render_template('index.html', users=users)
 
 
-@app.route("/account")
+@app.route("/blog")
+def blog():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('blogz.html', posts=posts)
+
+
+@app.route("/user/<string:username>")
 @login_required
-def account():
-    return render_template('account.html', title='Account')
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('singleUser.html', posts=posts, user=user)
 
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -45,20 +39,20 @@ def register():
         db.session.commit()
         flash('Your account has been created! You are now able to log in.', 'success')
         return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template('signup.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('newpost'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
+            login_user(user)
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
+            return redirect(next_page) if next_page else redirect(url_for('newpost'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -66,4 +60,21 @@ def login():
 @app.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('home'))
+    return redirect(url_for('blog'))
+
+@app.route("/newpost", methods=['GET', 'POST'])
+@login_required
+def newpost():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('blog'))
+    return render_template('create_post.html', title='New Post', form=form)
+
+@app.route("/blog/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
